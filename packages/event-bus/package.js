b989c91a4b518cb318d8cbaf7b38013e348552e4
Oualid Sellal <oualid.sellal@gmail.com>
2020-10-27 09:01:16 +0100

    update

--- packages/event-bus/package.js
+++ packages/event-bus/package.js
@@ -1,6 +1,1 @@
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'PROD') {
-	module.exports = require('./dist/micromodule.js');
-} else {
-	module.exports = require('./src/micromodule.js');
-}
+module.exports = require('./dist/micromodule.js');
