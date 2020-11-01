b23152f9ed8ad1343ec4d70d5a1d32c8d48472f3
Oualid Sellal <oualid.sellal@gmail.com>
2020-11-01 13:46:02 +0100

    update web-server

--- packages/web-server/src/micromodule.js
+++ packages/web-server/src/micromodule.js
@@ -2,13 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-export default class WebServerInfrastructureMicromodule {
-	constructor({ port }) {
-		this.port = port;
-		this.server = express();
-		this.server.use(bodyParser.urlencoded({ extended: false }));
-		this.server.use(bodyParser.json());
-		this.server.use(cors());
+export default class WebServer {
+	#validator;
+
+	#server;
+
+	#port;
+
+	constructor({ port, dependencies }) {
+		if (!dependencies) throw new Error('Dependencies undefined');
+		if (!dependencies.validator) throw new Error('Validator undefinec');
+		this.#validator = dependencies.validator;
+		this.#port = port;
+		this.#server = express();
+		this.#server.use(bodyParser.urlencoded({ extended: false }));
+		this.#server.use(bodyParser.json());
+		this.#server.use(cors());
 	}
 
 	register({ routes, controllers, middlewares }) {
@@ -24,14 +33,27 @@ export default class WebServerInfrastructureMicromodule {
 			const controller = async (req, res) => {
 				try {
 					const request = req;
-					request.params = req.body;
+					if (route.params) {
+						const schema = route.params;
+						if (!schema.$$strict) schema.$$strict = 'remove';
+						switch (route.method) {
+							case 'post':
+								await this.#validator.validate({ data: request.body, schema });
+								break;
+							case 'get':
+								await this.#validator.validate({ data: request.params, schema });
+								break;
+							default:
+								break;
+						}
+					}
 					const response = await handler({ request });
 					if (response instanceof Error || (response?.stack && response?.message)) {
 						const error = {
 							status: 'error',
 							timestamp: new Date(),
 							payload: {
-								source: 'http-api',
+								source: 'web-server',
 								route: route.name,
 								reasons:
 									response.name === 'VALIDATION_ERROR'
@@ -47,7 +69,7 @@ export default class WebServerInfrastructureMicromodule {
 						status: 'error',
 						timestamp: new Date(),
 						payload: {
-							source: 'http-api',
+							source: 'web-server',
 							route: route.name,
 							reasons: [error.name],
 						},
@@ -65,13 +87,13 @@ export default class WebServerInfrastructureMicromodule {
 					routeMiddlewares.push(middlewareHandler);
 				});
 			}
-			this.server[route.method.toLowerCase()](route.path, routeMiddlewares, controller);
+			this.#server[route.method.toLowerCase()](route.path, routeMiddlewares, controller);
 		});
 	}
 
 	run() {
 		return new Promise((resolve) => {
-			const { port } = this;
+			const port = this.#port;
 			const successEvent = {
 				status: 'success',
 				timestamp: new Date(),
@@ -79,7 +101,7 @@ export default class WebServerInfrastructureMicromodule {
 					port,
 				},
 			};
-			this.server.listen(port, () => {
+			this.#server.listen(port, () => {
 				resolve(successEvent);
 			});
 		});
